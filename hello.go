184142cd5c0134a0d84ce4298878acfb7f0c3cf8
Greeneyesredmetal <Erangluang@163.com>
2019-09-06 14:33:34 +0800

    第二次提交

--- hello.go
+++ hello.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 )
 
-func main(){
-	fmt.Println("hello,world!")
+type notifier interface {
+	notify()
 }
 
+type user struct {
+	name  string
+	email string
+}
+
+func (u user) notify() {
+	fmt.Println(u.name)
+	fmt.Println(u.email)
+}
+
+func main() {
+	fmt.Println("hello,world!")
+	bill := user{"Bill", "Bill@ink.com"}
+	bill.notify()
+}
