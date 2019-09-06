6cf6ef4c57f522ff1dad7c241f443e78547adacc
Green bump <Erangluang@163.com>
2019-09-06 14:49:43 +0800

    Update hello.go
    增加了新的结构体

--- hello.go
+++ hello.go
@@ -13,9 +13,18 @@ type user struct {
 	email string
 }
 
+type admin struct {
+	name  string
+	email string
+}
+
 func (u user) notify() {
 	fmt.Println(u.name)
 	fmt.Println(u.email)
+}
+
+func (a admin) notify() {
+	fmt.Println("不允许查看管理员通知信息")
 }
 
 func main() {
